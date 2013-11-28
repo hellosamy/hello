56a31011028f7c9f9012dfcb306998bb436457b7
hellosamy2005 <hellosamy2005@gmail.com>
2013-11-28 17:25:04 +0800

    Update evil.js

--- evil.js
+++ evil.js
@@ -28,7 +28,7 @@ function sendCommentFromPageRegExp(url,regExpStr){
             var  sendurl="/office/cucCard/cucCardAction_queryReplyByPage.so?sendId="+result[1];
             $.getJSON(sendurl,function(jsonData){
                 if(jsonData.replyList.length<=0){
-                   $.post("/office/cucCard/cucCardAction_reply.so",{sendId:result[1],content:" ",ifSendCard:"00"});
+                   $.post("/office/cucCard/cucCardAction_reply.so",{sendId:jsonData.sendId,content:" ",ifSendCard:"00"});
                 }
             });
         }
